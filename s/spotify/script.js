6ce3ff92f93b7b8af4aa8fc6265b991b74f3bb2e
Ashutosh Singh <1901330310038@niet.co.in>
2023-01-20 01:37:55 +0530

    new feature devloped

--- s/spotify/script.js
+++ s/spotify/script.js
@@ -1,2 +1,10 @@
 console.log("welcome to spotify");
 let a=new Audio('1.mp3');
+
+
+
+// mai aaj ek naya feature bana ke upload karunga/karungi
+
+//maine feature bana diya hai 
+
+//  now its time to upload
